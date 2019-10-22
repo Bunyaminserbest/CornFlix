8f87e26c196f2ad65059644499d308fc389f4395
Serbest <bunyamin.srbst@gmail.com>
2019-10-22 12:23:50 +0200

    FunctionPDO2

--- model/model.php
+++ model/model.php
@@ -1,6 +1,12 @@
 <?php
 function connect_db(){
-
-$bdd =new PDO('mysql:host=localhost2:3306;dbname=id11312830_cornflix', 'root', '');
+  try
+  {
+    $bdd =new PDO('mysql:host=localhost2:3306;dbname=id11312830_cornflix', 'root', '');
+  }
+  catch(Exception $e)
+  {
+          die('Erreur : '.$e->getMessage());
+  }
 }
 ?>
